dc6ce9d7d32620362119ec8944ade0e8235bfb1c
legacyKim <123148778+legacyKim@users.noreply.github.com>
2025-01-14 01:47:08 +0900

    build

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from 'react';
 import { fetchProjects } from '../api/projects/api.js';
 
+import Image from 'next/image';
+
 interface Project {
     imgsrc: string;
     project: string;
@@ -43,7 +45,12 @@ export default function Project() {
                         <li key={p.project}>
                             <a href={p.link}>
                                 <div className='project_img'>
-                                    <img src={p.imgsrc} alt={p.project} />
+                                    <Image
+                                        src={p.imgsrc} 
+                                        alt={p.project}
+                                        width={500}
+                                        height={300}
+                                    />
                                     <svg stroke="#fff" fill="#fff" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>
                                     </svg>
