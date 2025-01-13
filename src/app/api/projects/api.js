67675d688360892aae7f7d60caa5f95e901bdf1a
legacyKim <123148778+legacyKim@users.noreply.github.com>
2025-01-14 01:56:08 +0900

    build

--- src/app/api/projects/api.js
+++ src/app/api/projects/api.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
 import axios from 'axios';
+
+dotenv.config();
 
 export const fetchProjects = async () => {
     try {
-        const response = await axios.get('/api/projects');
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`);
         return response.data;
     } catch (err) {
         console.error(err);
