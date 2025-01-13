96926cd02d8d04cbe3db309fc9e327403ede8b15
legacyKim <123148778+legacyKim@users.noreply.github.com>
2025-01-14 05:05:42 +0900

    db test

--- src/app/api/projects/db.js
+++ src/app/api/projects/db.js
@@ -3,17 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const host = process.env.MYSQL_HOST;
-const user = process.env.MYSQL_USER;
-const password = process.env.MYSQL_PASSWORD;
-const database = process.env.MYSQL_DATABASE;
-
-const pool = mysql.createPool({
-    host: host,            
-    user: user,            
-    password: password,    
-    database: database,
-    port: 3306,
-});
+const mysqlUrl = process.env.MYSQL_URL;
+const pool = mysql.createPool(mysqlUrl);
 
 export const promisePool = pool.promise();
