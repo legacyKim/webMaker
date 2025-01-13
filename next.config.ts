3d75af08c8d37e952bfc2fde23d9a142adf0e58f
legacyKim <123148778+legacyKim@users.noreply.github.com>
2025-01-14 02:01:12 +0900

    build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
-
-export default nextConfig;
