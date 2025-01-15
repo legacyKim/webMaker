e2f90f29c58a998e6050284cc857e1b2ba3f9df7
legacyKim <123148778+legacyKim@users.noreply.github.com>
2025-01-15 09:11:05 +0900

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['idea-portfolio-new-2025.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
